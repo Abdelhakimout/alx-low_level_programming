12808952b90b0bc1880604c2f958e49a2995dbee
Abdelhakimout <abdelhakimoutemma@gmail.com>
2023-06-17 13:14:16 -0700

    com 5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -3,7 +3,7 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Return: Always 0 (Succes)
  */
 
 int main(void)
@@ -12,38 +12,33 @@ int main(void)
 	int hun;
 	int ten;
 	int bas;
-	
-	for (tho = 0; tho < 10; tho+=)
+
+for ( tho = 0 ; tho < 10 ; tho++)
+{	
+
+	for (hun = 0 ; hun <= 10 ; hun++)
 	{
-		for (hun = 0; hun < 10; hun++)
+	
+		for (ten = 0 ; ten < 10 ; ten++)
 		{
-            for (ten = tho; ten < 10; ten++)
-            {
-                for (bas = 0; bas < 10; bas++)
-                {
-                    int num1 = (tho * 10) + hun;
-                    int num2 = (ten * 10) + bas;
-
-                    if (num1 != num2)
-                    {
-                        putchar((tho + '0'));
-                        putchar((hun + '0'));
-                        putchar(' ');
-                        putchar((ten + '0'));
-                        putchar((bas + '0'));
-
-                        if (tho != 9 || hun != 9 || ten != 9 || bas != 9)
-                        {
-                            putchar(',');
-                            putchar(' ');
-                        }
-                    }
-                }
-            }
-        }
-    }
-
-    putchar('\n');
-    return 0;
+			for ( bas = 0 ; bas < 10 ; bas++)
+			{
+				putchar('0' + tho);
+				putchar('0' + hun);
+				putchar('');
+				putchar('0' + ten);
+				putchar('0' + bas);
+				if (!(tho == 9 && hun == 9))
+				{
+					putchar(',');
+					putchar();
+				}
+				bas++;
+			}
+		}
+	}
+}
+	putchar('\n');
+	return (0);
 }
 
