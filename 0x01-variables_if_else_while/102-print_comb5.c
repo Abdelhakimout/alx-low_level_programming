5c215764cd9b69bd33b4604873da889b475087b8
Abdelhakimout <abdelhakimoutemma@gmail.com>
2023-06-17 12:56:53 -0700

    com 5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -3,41 +3,45 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 (Succes)
+ * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-	int tho;
-	int hun;
-	int ten;
-	int bas;
+    int tens1, ones1;
+    int tens2, ones2;
 
-for (tho = 0 ; tho < 10 ; tho++)
-{
+    for (tens1 = 0; tens1 <= 9; tens1++)
+    {
+        for (ones1 = 0; ones1 <= 9; ones1++)
+        {
+            for (tens2 = tens1; tens2 <= 9; tens2++)
+            {
+                for (ones2 = 0; ones2 <= 9; ones2++)
+                {
+                    int num1 = (tens1 * 10) + ones1;
+                    int num2 = (tens2 * 10) + ones2;
 
-	for (hun = 0 ; hun <= 10 ; hun++)
-	{
+                    if (num1 != num2)
+                    {
+                        putchar((tens1 + '0'));
+                        putchar((ones1 + '0'));
+                        putchar(' ');
+                        putchar((tens2 + '0'));
+                        putchar((ones2 + '0'));
 
-		for (ten = 0 ; ten < 10 ; ten++)
-		{
-			for (bas = 0 ; bas < 10 ; bas++)
-			{
-				putchar('0' + tho);
-				putchar('0' + hun);
-				putchar(32);
-				putchar('0' + ten);
-				putchar('0' + bas);
-				if (!(tho == 9 && hun == 8))
-				{
-					putchar(',');
-					putchar(32);
-				}
-			}
-		}
-	}
-}
-	putchar('\n');
-	return (0);
+                        if (tens1 != 9 || ones1 != 9 || tens2 != 9 || ones2 != 9)
+                        {
+                            putchar(',');
+                            putchar(' ');
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    putchar('\n');
+    return 0;
 }
 
