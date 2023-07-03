771a741bc05b357595485ac499a0fdac9faa555d
Abdelhakimout <135274367+Abdelhakimout@users.noreply.github.com>
2023-07-03 23:08:54 +0100

    Update 9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,28 +1,30 @@
 #include "main.h"
 
 /**
-  * _strcpy - unction that copies the string pointed to by src
-  * including the terminating null byte (\0),
-  * to the buffer pointed to by dest.
-  *
-  * @dest: string to contain a copy of src content.
-  * @src: string to copy its content to dest.
-  *
-  * Return: pointer to dest.
-  */
+ * *_strcpy - copies the string pointed to by src
+ * including the terminating null byte (\0)
+ * to the buffer pointed to by dest
+ * @dest: pointer to the buffer in which we copy the string
+ * @src: string to be copied
+ *
+ * Return: the pointer to dest
+ */
 char *_strcpy(char *dest, char *src)
 {
-	int len = 0;
+	int len, i;
 
-	while (1)
+	len = 0;
+
+	while (src[len] != '\0')
 	{
-		*dest = *src;
 		len++;
-		if (*src == '\0')
-			break;
-		src++;
-		dest++;
 	}
-	dest = dest - (len - 1);
+
+	for (i = 0; i < len; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+
 	return (dest);
 }
