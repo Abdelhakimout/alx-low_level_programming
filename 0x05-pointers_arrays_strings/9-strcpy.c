b1fe3c4f4d52a1b9cafa6d9b05586a669635d349
Abdelhakimout <135274367+Abdelhakimout@users.noreply.github.com>
2023-07-04 00:27:52 +0100

    Update 9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,31 +1,22 @@
-#include "main.h"
+#include <stdio.h>
+#include <time.h>
 
-/**
- * *_strcpy - copies the string pointed to by src
- * including the terminating null byte (\0)
- * to the buffer pointed to by dest
- * @dest: pointer to the buffer in which we copy the string
- * @src: string to be copied
- *
- * Return: the pointer to dest
- */
-char *_strcpy(char *dest, char *src)
-{
-	int len, i;
+void generate_password(int length) {
+    char characters[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+{}[]<>?";
 
-	len = 0;
+    srand(time(NULL));
 
-	while (src[len] != '\0')
-	{
-		len++;
-	}
+    for (int i = 0; i < length; i++) {
+        printf("%c", characters[rand() % (sizeof(characters) - 1)]);
+    }
 
-	for (i = 0; i < len; i++)
-	{
-		dest[i] = src[i];
-	}
-	dest[i] = '\0';
-
-	return (dest);
+    printf("\n");
 }
 
+int main(void) {
+    
+ 
+    generate_password(15);
+
+    return 0;
+}
