5d3bcc8e87f8bdbb5a2ebf76a01317c010a5dfb3
Abdelhakimout <abdelhakimoutemma@gmail.com>
2023-07-18 14:25:27 -0700

    updated

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,22 +1,18 @@
 #include "main.h"
 #include <stdlib.h>
 #include <string.h>
-
 /**
  * strtow - Splits a string into words.
  * @str: The input string.
  * Return: Pointer to an array of strings (words), or NULL on failure
  */
-
 char **strtow(char *str)
 {
 	int i, j, k, len;
 	char **words = NULL;
 	int word_count = 0, in_word = 0;
-
 	if (str == NULL || *str == '\0')
 		return (NULL);
-
 	for (i = 0; str[i] != '\0'; i++)
 	{
 		if (str[i] != ' ' && !in_word)
@@ -28,18 +24,14 @@ char **strtow(char *str)
 			in_word = 0;
 		}
 	}
-
 	if (word_count == 0)
 		return (NULL);
-
 	words = (char **)malloc((word_count + 1) * sizeof(char *));
 	if (words == NULL)
 		return (NULL);
-
 	i = 0;
 	j = 0;
 	in_word = 0;
-
 	for (k = 0; k < word_count; k++)
 	{
 		while (str[i] == ' ')
@@ -47,7 +39,6 @@ char **strtow(char *str)
 		len = 0;
 		while (str[i + len] != ' ' && str[i + len] != '\0')
 			len++;
-
 		words[k] = (char *)malloc((len + 1) * sizeof(char));
 		if (words[k] == NULL)
 		{
@@ -56,7 +47,6 @@ char **strtow(char *str)
 			free(words);
 			return (NULL);
 		}
-
 		for (j = 0; j < len; j++)
 		{
 			words[k][j] = str[i];
@@ -64,8 +54,6 @@ char **strtow(char *str)
 		}
 		words[k][j] = '\0';
 	}
-
 	words[word_count] = NULL;
-
 	return (words);
 }
